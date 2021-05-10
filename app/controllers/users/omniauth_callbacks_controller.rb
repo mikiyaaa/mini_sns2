77711c430714796e9b42228bc0b47b6943b19ec9
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-05-10 23:50:55 +0900

    SNS認証でのログイン機能

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def authorization
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
+    if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
       sign_in_and_redirect @user, event: :authentication
     else
       render template: 'devise/registrations/new'
